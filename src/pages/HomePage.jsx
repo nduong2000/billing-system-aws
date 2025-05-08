bcdb792f7f5d885d8cafe1c3bc2ff6149e4a1cfa
Nick Duong <duong.nick@gmail.com>
2025-05-08 12:26:28 -0400

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,6 +5,12 @@ function HomePage() {
     <div>
       <h2>Welcome</h2>
       <p>Select an option from the navigation menu.</p>
+
+      <p>Still need to implement CloudFront for the backend API. For now check on the backend URL and accept the risk of bieing sign signed CERT.</p>
+      <br><a href="https://ec2-44-211-91-81.compute-1.amazonaws.com/" target="_blank">https://ec2-44-211-91-81.compute-1.amazonaws.com/</a></br>
+      
+      <p>Still need to implement CloudFront for the frontend API.</p>
+
     </div>
   );
 }
